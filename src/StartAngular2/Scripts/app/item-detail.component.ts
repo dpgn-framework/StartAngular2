72fd65b5d01b6d26fd36ca6ebe9ced5dcb7cd7f2
dpgn.framework@gmail.com <dpgn.framework@gmail.com>
2017-07-05 21:25:35 +0700

    Persisting CHANGES Add - Update - Delete items

--- src/StartAngular2/Scripts/app/item-detail.component.ts
+++ src/StartAngular2/Scripts/app/item-detail.component.ts
@@ -18,6 +18,16 @@ import { ItemService } from "./item.service";
                   <textarea [(ngModel)]="item.Description" placeholder="Insert a suitable description..."></textarea>
               </li>
           </ul>
+            <div *ngIf="item.Id == 0" class="commands insert">
+              <input type="button" value="Save" (click)="onInsert(item)" />
+              <input type="button" value="Cancel" (click)="onBack()" />
+            </div>
+
+            <div *ngIf="item.Id != 0" class="commands update">
+              <input type="button" value="Update" (click)="onUpdate(item)"/>
+              <input type="button" value="Delete" (click)="onDelete(item)"/>
+              <input type="button" value="Back" (click)="onBack()" />
+          </div>
         </div>
     `,
     styles: [`
@@ -51,9 +61,49 @@ export class ItemDetailComponent {
                 item => this.item = item
             );
         }
+        else if (id === 0) {
+            console.log("id is 0: adding a new item...");
+            this.item = new Item(0, "New Item", null);
+        }
         else {
             console.log("Invalid id: routing back to home...");
             this.router.navigate([""]);
         }
+    }
+
+    onInsert(item: Item) {
+        this.itemService.add(item).subscribe(
+            (data) => {
+                this.item = data;
+                console.log("Item " + this.item.Id + " has been added.");
+                this.router.navigate([""]);
+            },
+            (error) => console.log(error)
+        );
+    }
+
+    onUpdate(item: Item) {
+        this.itemService.update(item).subscribe(
+            (data) => {
+                this.item = data;
+                console.log("Item " + this.item.Id + " has been updated.");
+                this.router.navigate([""]);
+            },
+            (error) => console.log(error)
+        );
+    }
+    onDelete(item: Item) {
+        var id = item.Id;
+        this.itemService.delete(id).subscribe(
+            (data) => {
+                console.log("Item " + id + " has been deleted.");
+                this.router.navigate([""]);
+            },
+            (error) => console.log(error)
+        );
+    }
+
+    onBack() {
+        this.router.navigate([""]);
     }
 }
